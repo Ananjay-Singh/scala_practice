bd3288ecba457afa9418e962dfce8780a767c04e
ananjay.bigdata@gmail.com <ananjay.bigdata@gmail.com>
2022-02-11 00:27:00 +0530

    Added Loop

--- src/main/scala/com/ananjay/scala/practice/basic/Loop.scala
+++ src/main/scala/com/ananjay/scala/practice/basic/Loop.scala
@@ -1,7 +1,25 @@
 package com.ananjay.scala.practice.basic
-/*
 
- */
 object Loop extends App{
+  println("******* Table for 2 *********")
+  for(i <- 1 to 10){
+    println(s"2 X $i = ${2*i}")
+  }
 
+  println("***** Factorial Programme *****")
+  var num = 5;
+  var res = 1
+  while(num > 0){
+    res*=num
+    num-=1
+  }
+  println("Factorial of 5 : "+ res)
+
+  println("**** Sum of the digit Programme ****")
+  var sum = 0
+  do{
+    sum+=res%10
+    res/=10
+  }while(res>0)
+  println("Sum of the digit :"+sum)
 }
--- src/main/scala/com/ananjay/scala/practice/basic/Variables.scala
+++ src/main/scala/com/ananjay/scala/practice/basic/Variables.scala
@@ -3,11 +3,12 @@ package com.ananjay.scala.practice.basic
 Key Learning :
   1. Not possible of reassignment to val
   2. Var variable can be changed
-  3. We need not to be write the type explicitly It can be inferred by the compiler
+  3. We need not to be write the type explicitly It can be inferred by the compiler(Type Inference)
   4. Variable, Class, Object, Function name should be in CamelCase
   5. We need to give f at the end of the Float type variable
   6. We need to give l at the end of the Long type variable
   7. Semicolon(;) is not required and only required if multiple statement are there in single line.
+  8. Last statement is the return in block expression
  */
 object Variables extends App{
   var a = 5
@@ -29,4 +30,11 @@ object Variables extends App{
   val i:Byte = 123
   println("Byte : " +i)
   println("Combined Result : "+ a + b + c + d + e + f + g + h + i)
+
+  var y = {
+    var x= 10
+    x+5
+  }
+  println("Y = "+y)
+
 }
